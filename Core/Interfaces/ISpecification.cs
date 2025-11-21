1c4f60023ad9639e4c8f9bdb76825bca5c73a431
Aaron <a.a.r.g@outlook.com>
2025-11-21 04:06:45 -0600

    section5

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -10,7 +10,13 @@ public interface ISpecification<T>
     Expression<Func<T, object>>? OrderByDescending {get;}
 
     bool IsDistinct{get;}
-       
+
+    int Take {get;}
+
+    int Skip{get;}
+    bool IsPagingEnabled {get;}
+
+    IQueryable<T> ApplyCriteria(IQueryable<T> query);
 
 }
 
