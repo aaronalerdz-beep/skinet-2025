981ba6790f2f02648c1154be625b95a5e94131a7
Aaron <a.a.r.g@outlook.com>
2025-11-21 17:00:30 -0600

    errorhandle 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System.Drawing.Printing;
 using System.Net.Http.Headers;
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +16,13 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseMiddleware<ExceptionMiddlewar>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","http://localhost:4200"));
 app.MapControllers();
 
 try
