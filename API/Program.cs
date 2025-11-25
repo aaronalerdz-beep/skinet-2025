ea87efdf1eeaaef9461261102b4c3e13c7fa5545
Aaron <a.a.r.g@outlook.com>
2025-11-25 03:56:07 -0600

    Section 8

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionMiddlewar>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","http://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 try
---
+++ client/src/app/layout/header/header.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { MatBadgeModule} from '@angular/material/badge';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+
+}
