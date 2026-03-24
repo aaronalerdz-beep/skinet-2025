fac5ad46670c00ba88c0cfe897f41d72bc4d91df
Aaron <a.a.r.g@outlook.com>
2026-03-24 03:36:27 -0500

    3

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51TCx5XDn8OsuG0MI7u1V3k3Z2e550PrxP3wYtihsOrVXXfyY8hwayPhDk3x75tnLIlbkBSW9KTQ1Kn7bVW3KDyK9008RbjVx7c'
+    stripePublicKey: ''
 
 };
